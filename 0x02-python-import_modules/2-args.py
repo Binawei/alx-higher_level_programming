6514d67c9943bcfd1b976c9d4d14771ae67d8bc6
root <gbamilabina.gmail.com>
2022-12-02 12:23:38 +0000

    prints argument from the command line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    if argv_count is 0:
-        print("{0:.0f} arguments.".format(argv_count))
-    elif argv_count is 1:
-        print("{:.0f} argument:".format(argv_count))
-        print("{:.0f}: {:s}".format(index, sys.argv[1]))
+    
+    argv_count = len(sys.argv)
+    #index = 1
+    if argv_count is 1:
+        print("{:d} arguments.".format(0))
+    elif argv_count is 2:
+        print("{:.0f} argument:".format(1))
+        print("{:.0f}: {:s}".format(1, sys.argv[1]))
     else:
-        print("{:.0f} arguments:".format(argv_count))
-        while index <= argv_count:
+        print("{:.0f} arguments:".format(argv_count - 1))
+        for index in range(1, argv_count):
             print("{:.0f}: {:s}".format(index, sys.argv[index]))
             index += 1
 
