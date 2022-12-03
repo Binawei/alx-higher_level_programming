4709f8a8ffc3c5ce9911ec6f8c64002245d72e7d
root <gbamilabina.gmail.com>
2022-12-03 12:57:44 +0000

    replacses element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     over = len(my_list)
+    insert = my_list.insert(idx, element)
     if idx < 0:
         return my_list
-    elif idx > over:
+    elif idx >= over:
         return my_list
     else:
-        return my_list.insert(idx, element)
+        return insert
