f3d8b2bd1920ee4a14b63c3bd1769637ee9c79cd
root <gbamilabina.gmail.com>
2022-12-03 12:39:07 +0000

    replaces an element of a list at a specific position (like in C).

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     over = len(my_list)
     if idx < 0:
         return my_list
-    elif idx >= over:
+    elif idx > over:
         return my_list
     else:
         return my_list.insert(idx, element)
