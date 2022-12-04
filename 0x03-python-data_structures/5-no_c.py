df123b7a022a4fbd6c29755ad637a74585e828fd
root <gbamilabina.gmail.com>
2022-12-04 14:39:52 +0000

    removes all characters c and C from a string.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    #charcters to be removed stored in ch
     ch = ['c', 'C']
-    #to filter out the characters
     filtered_char = filter(lambda item: item not in ch, my_string)
     my_string = ''.join(filtered_char)
     print(my_string)
