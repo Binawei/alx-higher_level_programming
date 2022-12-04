d7a13a6c9555e2b86c9550e9bd41afe1ea437f2a
root <gbamilabina.gmail.com>
2022-12-04 14:48:42 +0000

    Removes the occurrence of C and c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    ch = ['c', 'C']
-    filtered_char = filter(lambda item: item not in ch, my_string)
-    my_string = ''.join(filtered_char)
-    print(my_string)
+    my_list = list(my_string)
+    for ch in my_list:
+        if ch == 'c' or ch == 'C':
+            my_list.remove(ch)
+    return "".join(my_list)
