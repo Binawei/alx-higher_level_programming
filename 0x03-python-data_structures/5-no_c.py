2c7e55f85d9cbaa92e378b812098a49cf3d95b7c
root <gbamilabina.gmail.com>
2022-12-04 14:53:18 +0000

    removes c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,3 +5,13 @@ def no_c(my_string):
         if ch == 'c' or ch == 'C':
             my_list.remove(ch)
     return "".join(my_list)
+
+
+
+print(no_c("Best School"))
+
+print(no_c("Chicago"))
+
+print(no_c("C is fun!"))
+
+
