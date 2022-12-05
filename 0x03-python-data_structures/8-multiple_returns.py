7baa0f3a65735c4ae269062159945e93f1dcc312
root <gbamilabina.gmail.com>
2022-12-05 13:16:06 +0000

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = "None"
+        sentence[0] = None
     return len(sentence), sentence[0]
-
-sentence = ""
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
