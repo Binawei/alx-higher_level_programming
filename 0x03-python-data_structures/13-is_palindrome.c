f7dadc915288ffefa4e4380a379de8dffc01b42a
root <gbamilabina.gmail.com>
2022-12-06 12:06:55 +0000

    palindrome list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,66 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "lists.h"
 /**
-* get_middle - finds a pointer to the middle of the given linked list
-* head: a pointer to the first node of the linked list
+* reverse - reverses a singly linked list
+* @head: a pointer to the first element
 *
-* Return: a pointer to the middle nide of the linked list
-* or null if it fails
+* Return: pointer to the first element of the reversed list
 */
-listint_t* get_middle(listint_t *head)
+listint_t *reverse(listint_t *head)
 {
-    if (head == NULL)
-        {
-            return (head);
-        }
-    listint_t *fast = head; /*to traverse by two step*/
-    listint_t *slow = head; /*to traverse by one step*/
-    
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
-        slow = slow->next;
-    }
-    return (slow);
-}
+    listint_t *prev = NULL;
+    listint_t *next_ptr = NULL;
 
-/**
-* reverse - reverses the last half of the linked list
-* @head: pointer to the first node in the list
-*
-* Return: a poiter to the first value of the halved list
-*/
-listint_t* reverse(listint_t *head)
-{
-    if (head == NULL || head->next == NULL)
+    while (head != NULL)
     {
-        return (head);
+        next_ptr = head->next;
+        head->next = prev;
+        prev = head;
+        head = next_ptr;
     }
-    listint_t *temp = reverse(head->next);
-    head->next->next = head;
-    head->next = NULL;
-    return temp;
+    return (prev);
 }
-
 /**
 * is_palindrome - checks if a singly linked list is a palindrome.
-* @head: pointer of pointer to the first element of the linked list
+* @head: pointer of pointer to the first element of the list
 *
 * Return: 0 if it is not a palindrome, 1 if it is a palindrome
 */
 int is_palindrome(listint_t **head)
 {
-    listint_t *mid = get_middle(*head);
-    mid = reverse(mid);
-    while (mid != NULL)
+    if (head == NULL || *head.next == NULL)
+        return 1;
+    listint_t **slow = head;
+    listint_t **fast = head;
+
+    /*find the middle of the list*/
+    while (fast->next != NULL && fast->next->next != NULL)
     {
-        if (head->n != mid->n)
-            {
-                return (0);
-            }
-        head = head->next;
-        mid = mid->next;
+        slow = slow->next;
+        fast = fast->next->next;
     }
-    return (1);
+    slow->next = reverse(slow->next);
+    slow = slow->next;
+    /*check if the left half is equall to right half*/
+    while (slow != NULL)
+    {
+        if (head->n != slow->n)
+        {
+            return (0);
+        }
+        head = head->next;
+        slow = slow->next;
+    }
+     return (1);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -18,5 +18,4 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 listint_t *reverse(listint_t *head);
-listint_t *get_middle(listint_t *head);
 #endif /* LISTS_H */
