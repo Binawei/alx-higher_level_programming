a37cc91ea0cd717ce49cec3749b244d9603e23d5
root <gbamilabina.gmail.com>
2022-12-20 14:42:20 +0000

    prints an integer.

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+    except exception as i:
+        sys.stderr.write("EXception: {}\n".format(i))
+        return False
+    else:
+        rerurn True
+         
