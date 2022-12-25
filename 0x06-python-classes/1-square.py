50a33ae34ac8934cbe576b1f7b561c294d701056
root <gbamilabina.gmail.com>
2022-12-25 21:01:37 +0000

    a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+"""
+
+circle.py: The circle module, which defines a Circle class.
+
+"""
 class Square:
+ """A Square instance models a circle with a radius"""
     def __init__(self, size):
+    """Initializer with default size as private"""
         self.__size = size
+        """Private instance attribute: size"""
