c02f0e961782d100cd88bc3e7c4da43e8313e7d4
root <gbamilabina.gmail.com>
2023-01-12 21:30:31 +0000

    calculates area and perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""a class Rectangle that defines a rectangle by:
+(based on 0-rectangle.py)"""                                                    
+class Rectangle:                                                                
+    """a class Rectangle that defines a rectangle by"""         
+    def __init__(self, width = 0, height = 0):                                  
+        self.width = width                                                 
+        self.height = height                                 
+
+    @property                                                                                      
+    def width(self):                                              
+        """A method to retrieve the width using the knowledge 
+        of getter"""                           
+        return self.__width                                                 
+
+    @width.setter    
+
+    def width(self, value):                                                                                                             
+        """A method to sets the value of the width to value"""                                                                          
+        if type(value) != int:                                             
+            raise TypeError("width must be an integer")         
+        if value < 0:                                            
+            raise ValueError("width must be >= 0")                             
+        self.__width = value 
+
+    def area(self):
+        """A method to return the rectangle area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
