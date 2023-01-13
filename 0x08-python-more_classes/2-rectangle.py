ee8ec77ca842c05631a9055985bb7ddd139bb310
root <gbamilabina.gmail.com>
2023-01-13 06:30:52 +0000

    prints rectangle using # in string

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -30,5 +30,5 @@ class Rectangle:
     def perimeter(self):
         """Returns the perimeter of the rectangle"""
         if self.width == 0 or self.height == 0:
-            return 0
+            return 
         return 2 * (self.width + self.height)
