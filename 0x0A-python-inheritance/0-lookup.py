0853cbccc4b19044de94966f5b4d1b454859e098
root <gbamilabina.gmail.com>
2023-01-16 10:24:06 +0000

    list of available attributes and methods of an object:

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a function that returns the list of available attributes
+and methods of an object:"""
+def lookup(obj):
+    """the function to return the list usin the dir() function"""
+    return dir(obj)
