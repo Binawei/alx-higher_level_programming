5f408d655b3ad88051d006ca3efcb5fca1f96ed3
root <gbamilabina.gmail.com>
2023-01-16 19:18:40 +0000

    a public instance with exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry.
+creates a class
+"""
+class BaseGeometry:
+    """
+    class with public instance method.
+    """
+    def area(self):
+        """
+        raises an Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
