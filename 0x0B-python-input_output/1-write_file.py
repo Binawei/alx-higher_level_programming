de5fb6d484b6fd4d66aa265a0aa9138bb6ace6ea
root <gbamilabina.gmail.com>
2023-01-18 17:20:56 +0000

    writes to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 1-write_file.py.
+writes a string to a text file (UTF8) and returns the number of characters written:
+"""
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written:
+    args:
+        -filename: name of the file
+        -text: the texts to be written on the file
+    """
+    with open(filename, 'w+') as f:
+        return f.write(text)
