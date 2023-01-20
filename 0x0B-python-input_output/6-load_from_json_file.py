feabf44026d889816819230f3037dcb279bf9f68
root <gbamilabina.gmail.com>
2023-01-20 15:16:35 +0000

    creates object

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""
-a function that creates an Object from a "JSON file�":
+""" Module that creates an object from a JSON file
 """
 import json
 def load_from_json_file(filename):
-    """creates an Object from a “JSON file”:
-    args: filename: the file to create the object from
+    """function tht creates an object from a JSON file:
+    args:
+        filename: the file to create the object from
+    Raises:
+        Exception: when the object can't be encoded
     """
     with open(filename, 'r', encoding="UTF-8") as f:
         return json.load(f)
