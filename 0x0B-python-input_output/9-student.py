d7a0fc933883fd9c471ae6b72324c904789fcdc0
root <gbamilabina.gmail.com>
2023-01-20 16:13:40 +0000

    class of student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+class Student that defines a student by: (based on 9-student.py)
+"""
+class Student:
+    """
+    class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+         Public instance attributes of names and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation of a Student instance
+        Returns: the dict representation of the instance
+        """
+        return self.__dict__
