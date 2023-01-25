357a5ad947a95f990ca05dee3d2c13b3ad57488d
root <gbamilabina.gmail.com>
2023-01-25 15:58:18 +0000

    a class of linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -75,32 +75,4 @@ class SinglyLinkedList:
         while temp is not None:
             values.append(str(temp.data))
             temp = temp.next_node
-        return ('\n'.join(values))
-
-
-
-sll = SinglyLinkedList()
-
-sll.sorted_insert(2)
-
-sll.sorted_insert(5)
-
-sll.sorted_insert(3)
-
-sll.sorted_insert(10)
-
-sll.sorted_insert(1)
-
-sll.sorted_insert(-4)
-
-sll.sorted_insert(-3)
-
-sll.sorted_insert(4)
-
-sll.sorted_insert(5)
-
-sll.sorted_insert(12)
-
-sll.sorted_insert(3)
-
-print(sll)    
+        return ('\n'.join(values))   
