48e16e5163bbef74ad9757d25bec6cd9e50f240d
root <gbamilabina.gmail.com>
2023-01-25 09:34:48 +0000

    sets and retrieve the value of size

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,38 @@
-#!/usr/bin/python3
-"""A class called Square"""
-class Square:
-    """A class with a private object attribute"""
-    def __init__(self, size = 0):
-        """Method to initialize the square object
-        """
-        if not isinstance(size, int):
+#!/usr/bin/python3 
+"""This module a class Square that defines a square with                                                                                
+Private instance attribute: size
+"""                                                                                                                                     
+class Square:                                                                                                                           
+    """a class Square that defines a square"""
+    def __init__(self, size=0):                                                                                                         
+        """Instantiates with a private attribute size.
+        args:                                                                                                                           
+            size: size of the sqaure which must be >0 and of type int.
+        """                                                                   
+        if type(size) != int:                                                                                                           
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif size < 0:                                                                                                                  
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
+
     def area(self):
-        """Method that returns the area of the square
         """
+        Returns the current square area.                                                                                                
+        args:                                                                                                                           
+            self: the calling instance                                                                                                  
+        """                                                                                                                             
         return self.__size ** 2
+
     @property
     def size(self):
-        """ This method returns the size value
-        """
+        """Retrieves the size of the square so returns it"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        method to set the size value of the square
-        """
-        if not isinstance(value, int):
+        """Sets the value of the size to value"""
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("size ust be >= 0")
-        else:
-            self.__size = value
+            raise ValueError("size must be >= 0")
+        self.__size = value
