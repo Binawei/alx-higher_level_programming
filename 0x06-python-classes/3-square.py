0651da9b8459767fafc37a6afbc6d492d2238b07
root <gbamilabina.gmail.com>
2023-01-25 09:09:15 +0000

    square class with area method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,20 +1,25 @@
-#!/usr/bin/python3
-"""
-    defines a class called Square
-"""
-class Square:
-    """
-        A Square class with a private object attribute.
-        check the type of argument and raises error
-    """
-    def __init__(self, size = 0):
-        if type(size) is not int:
-             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+#!/usr/bin/python3                                                                                                                      
+"""This module a class Square that defines a square with                                                                                
+Private instance attribute: size                                                                                                        
+"""                                                                                                                                     
+class Square:                                                                                                                           
+    """a class Square that defines a square"""                                                                                          
+    def __init__(self, size=0):                                                                                                         
+        """Instantiates with a private attribute size.                                                                                  
+        args:                                                                                                                           
+            size: size of the sqaure which must be >0 and of type int.                                                                  
+        """                                                                                                                             
+        if type(size) != int:                                                                                                           
+            raise TypeError("size must be an integer")                                                                                  
+        elif size < 0:                                                                                                                  
+            raise ValueError("size must be >= 0")                                                                                       
         self.__size = size
-    """
-        A method that returns the area of the square
-    """
+
     def area(self):
+        """
+        Returns the current square area.
+        args:
+            self: the calling instance
+        """
         return self.__size ** 2
+
