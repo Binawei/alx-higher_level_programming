cc0477646e2f6c2f19e8ae54a3a8ff055b23cd16
root <gbamilabina.gmail.com>
2023-02-06 14:19:23 +0000

    Overides the area() from the parents class

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,33 +1,26 @@
-#!/usr/bin/python3                                                                                                                      
-"""Module 8-rectangle. create a rectangle class"""                  
+#!/usr/bin/python3
+"""this module writes a class Rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry                                      
-class Rectangle(BaseGeometry):                                                                                                          
-    """Represents a rectangle. private instance attributes:                                                                             
-        -width                                                                                                                          
-        -height                                                                                                                         
-    inherits from BaseGeometry.                                                                                                         
-    """                                                                                                                                 
-    def __init__(self, width, height):                                                                                                  
-        """Initializes an instance.                                                                                                     
-        Args:                                                                                                                           
-        -width: width of the rectangle                                                                                                  
-        -height: height of the rectangle                                                                                                
-        """                                                                                                                             
-        self.__width = width                                                                                                            
-        self.__height = height                                                                                                          
-        self.integer_validator("width", width)                                                                  
+class Rectangle(BaseGeometry):
+    """Represents a rectangle
+    privares instance attributes are;
+        -width: width of the rectangle
+        -height: height of the rectangle
+    public method area() inherited from BaseGeometty
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
         self.integer_validator("height", height)
 
+        self.__width = width
+        self.__height = height
+
+
     def area(self):
-        """Computes the area of the Rectangle
-        and overright the area from BaseGeometry
-        """
-        return self.__width * self.__height
+        """Area that overides the Geometry area method"""
+        return self.__height * self.__width
+
 
     def __str__(self):
-        """
-        returns a formatted string.
-        """
-        return str("[Rectangle] {} / {}".format(self.__width, self.__height)
-
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
